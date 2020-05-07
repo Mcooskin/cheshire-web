d73bc9a031416e4354af0e0f5cd9ae04e186a3be
Indre Makauskaite <makauskaite.indre@gmail.com>
2020-05-07 16:10:49 +0300

    vars.scss added, container tag added, reviews section with swiper

--- app/script.js
+++ app/script.js
@@ -1,1 +1,35 @@
 'use strict';
+
+// Initializing swiper for reviews section
+
+var reviewSwiper = new Swiper ('.swiper-container', {
+    direction: 'horizontal',
+    loop: false,
+    slidesPerView: 1,
+    spaceBetween: 120,
+    autoplay: {
+        delay: 1000,
+        disableOnInteraction: true,
+    },
+    breakpoints: {
+        700: {
+            slidesPerView: 2,
+            slidesPerGroup: 2,
+        },
+        1000: {
+            slidesPerView: 3,
+            slidesPerGroup: 3,
+        },
+    },
+
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+
+    // // Navigation arrows
+    // navigation: {
+    //   nextEl: '.swiper-button-next',
+    //   prevEl: '.swiper-button-prev',
+    // },
+})
